287d01e5a9a2501649b8db81668c70edbc1815fe
Aayush <thakur29aayush@gmail.com>
2026-06-18 14:03:19 +0545

    small cuts-02

--- client/src/context/ThemeContext.jsx
+++ client/src/context/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext(null);
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
